c650adcf924eaaa639b9c10ae32b7eb962a7a59f
Mark <mark.c@liveswitch.com.au>
2026-09-08 12:36:10 +0800

    Update sops-data.js

---
+++ sops-data.js
@@ -0,0 +1,242 @@
+const SOPS_DATA = {
+ "sites": {
+  "Google": {
+   "name": "Google",
+   "verification": "BDM Email Verification",
+   "url": "google.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Apple": {
+   "name": "Apple",
+   "verification": "Phone & Email",
+   "url": "businessconnect.apple.com",
+   "steps": [],
+   "notes": ""
+  },
+  "LinkedIn": {
+   "name": "LinkedIn",
+   "verification": "BDM Email Verification",
+   "url": "linkedin.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Facebook": {
+   "name": "Facebook",
+   "verification": "Phone & Email",
+   "url": "facebook.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Bing": {
+   "name": "Bing",
+   "verification": "BDM Email Verification",
+   "url": "bingplaces.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Yelp": {
+   "name": "Yelp",
+   "verification": "Phone Verification",
+   "url": "biz.yelp.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Foursquare": {
+   "name": "Foursquare",
+   "verification": "BDM Email Verification",
+   "url": "foursquare.com",
+   "steps": [],
+   "notes": ""
+  },
+  "American Express": {
+   "name": "American Express",
+   "verification": "Verification",
+   "url": "americanexpress.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Here": {
+   "name": "Here",
+   "verification": "BDM Email Verification",
+   "url": "here.com",
+   "steps": [],
+   "notes": ""
+  },
+  "TomTom": {
+   "name": "TomTom",
+   "verification": "BDM Email Verification",
+   "url": "tomtom.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Nextdoor": {
+   "name": "Nextdoor",
+   "verification": "BDM Email Verification",
+   "url": "nextdoor.com",
+   "steps": [],
+   "notes": ""
+  },
+  "True Local": {
+   "name": "True Local",
+   "verification": "BDM Email Verification",
+   "url": "truelocal.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Health Direct": {
+   "name": "Health Direct",
+   "verification": "Email Verification",
+   "url": "healthdirect.gov.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Kompass": {
+   "name": "Kompass",
+   "verification": "BDM Email Verification",
+   "url": "kompass.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Local Search": {
+   "name": "Local Search",
+   "verification": "Email Verification",
+   "url": "localsearch.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Yahoo": {
+   "name": "Yahoo",
+   "verification": "Email Verification",
+   "url": "yahoo.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Infobel": {
+   "name": "Infobel",
+   "verification": "BDM Email Verification",
+   "url": "infobel.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Health Engine": {
+   "name": "Health Engine",
+   "verification": "Email Verification",
+   "url": "healthengine.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Word of Mouth": {
+   "name": "Word of Mouth",
+   "verification": "BDM Email Verification",
+   "url": "wordofmouth.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Cybo": {
+   "name": "Cybo",
+   "verification": "BDM Email Verification",
+   "url": "cybo.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Yellow Pages": {
+   "name": "Yellow Pages",
+   "verification": "Phone Verification",
+   "url": "yellowpages.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "What Clinic": {
+   "name": "What Clinic",
+   "verification": "Phone Verification",
+   "url": "whatclinic.com",
+   "steps": [],
+   "notes": ""
+  },
+  "White Pages": {
+   "name": "White Pages",
+   "verification": "Phone Verification",
+   "url": "whitepages.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "StartLocal": {
+   "name": "StartLocal",
+   "verification": "BDM Email Verification",
+   "url": "startlocal.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "VY Maps": {
+   "name": "VY Maps",
+   "verification": "BDM Email Verification",
+   "url": "vymaps.com",
+   "steps": [],
+   "notes": ""
+  },
+  "Health Share": {
+   "name": "Health Share",
+   "verification": "BDM Email Verification",
+   "url": "healthshare.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Top Rated Online": {
+   "name": "Top Rated Online",
+   "verification": "BDM Email Verification",
+   "url": "top-rated.online",
+   "steps": [],
+   "notes": ""
+  },
+  "My Community Directory": {
+   "name": "My Community Directory",
+   "verification": "BDM Email Verification",
+   "url": "mycommunitydirectory.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "My Health 1st": {
+   "name": "My Health 1st",
+   "verification": "BDM Email Verification",
+   "url": "myhealth1st.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Three Best Rated": {
+   "name": "Three Best Rated",
+   "verification": "BDM Email Verification",
+   "url": "threebestrated.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Healthcare Link": {
+   "name": "Healthcare Link",
+   "verification": "BDM Email Verification",
+   "url": "healthcarelink.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Aus Health Pages": {
+   "name": "Aus Health Pages",
+   "verification": "Phone Verification",
+   "url": "aushealthpages.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Fixed Dental": {
+   "name": "Fixed Dental",
+   "verification": "No Verification",
+   "url": "fixeddental.com.au",
+   "steps": [],
+   "notes": ""
+  },
+  "Dentist.com.au": {
+   "name": "Dentist.com.au",
+   "verification": "BDM Email Verification",
+   "url": "dentist.com.au",
+   "steps": [],
+   "notes": ""
+  }
+ }
+};
